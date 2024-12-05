86cf5eb17ac5fcfe767da424b9b2c779ddb478e3
Mayur Shah <shahmayur001@gmail.com>
2024-12-05 16:12:02 +0530

    Update gem to support the latest Solidus version(4.4)

--- app/overrides/views/admin_users_subscriptions_tab.rb
+++ app/overrides/views/admin_users_subscriptions_tab.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
-Deface::Override.new(
-  virtual_path: 'spree/admin/users/_tabs',
-  name: 'solidus_subscriptions_admin_users_subscriptions_tab',
-  insert_bottom: "[data-hook='admin_user_tab_options']",
-  partial: 'spree/admin/users/subscription_tab'
-)
+module Views
+  module AdminUsersSubscriptionsTab
+    Deface::Override.new(
+      virtual_path: 'spree/admin/users/_tabs',
+      name: 'solidus_subscriptions_admin_users_subscriptions_tab',
+      insert_bottom: "[data-hook='admin_user_tab_options']",
+      partial: 'spree/admin/users/subscription_tab'
+    )
+  end
+end
